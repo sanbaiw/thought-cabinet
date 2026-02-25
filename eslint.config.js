69467de1b81ae1bc9c357c09179f4bdcb2090580
sanbaiw <wangzhihao1030@gmail.com>
2026-02-26 00:01:27 +0800

    chore: Cleanup packaging and denpendencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import tseslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import prettierConfig from 'eslint-config-prettier'
-import prettierPlugin from 'eslint-plugin-prettier'
 import js from '@eslint/js'
 
 export default [
@@ -44,14 +43,12 @@ export default [
     },
     plugins: {
       '@typescript-eslint': tseslint,
-      prettier: prettierPlugin,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
       ...prettierConfig.rules,
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
-      'prettier/prettier': 'error',
     },
   },
 ]
